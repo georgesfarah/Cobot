1429fa27a1f841f3cc1f3cdfc4c00b40ddcd94c1
Radwan Afiouni <49821109+RadwanAfiouni@users.noreply.github.com>
2021-07-08 13:09:13 +0300

    oui

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import raspberry_main
 
 
 collection=int(input('number of collections '))
-number_of_testing_points=int(input('number of testing points '))
 number_of_mes=10
 
 for sections in range(collection):
     collection_name=str(input('collection name'))
+    number_of_testing_points=int(input('number of testing points '))
     for i in range(number_of_testing_points):
         ID=str(input("ID "))
         firestore_main.add_rssi(raspberry_main.get_apinfo(number_of_mes),ID,collection_name)
